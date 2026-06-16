d1e036de0bc3965c8088647254f6e1d388622b29
drfood700-spec <dr.food700@gmail.com>
2026-06-16 19:54:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -117,7 +117,8 @@ def main():
     application = Application.builder().token(BOT_TOKEN).build()
     
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("admin", admin_panel))
+   application.add_handler(CommandHandler("panel", admin_panel))
+
     application.add_handler(CallbackQueryHandler(button_handler))
     
     application.run_polling()
